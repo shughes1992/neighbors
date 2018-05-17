e890153cf8885bf3406ea52d5ca9a9ce0e988639
Samantha Hughes <hughes2@law.upenn.edu>
2018-05-16 22:15:47 -0400

    editing CSS properties withing React.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import background from './images/neighbors-1.jpg'; 
+import background from './images/neighbors-2.jpg'; 
 import './App.css';
 
 class App extends Component {
@@ -8,8 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <h1 className="App-title">Neighbors</h1>
           <img src={background} className="App-background" alt="neigborhood" style={{height: 700, opacity: 0.5}}/>
-          <h1 className="App-title">Neighbors</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
