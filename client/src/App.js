868943e5fc46fe601c5e7c7262432f38b560193c
Samantha Hughes <hughes2@law.upenn.edu>
2018-05-17 17:56:52 -0400

    adding login / sign-up form

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import background from './images/neighbors-2.jpg'; 
+// import '../public/stylesheets/style.css'
+import background from './components/assets/images/neighbors-2.jpg'; 
 import './App.css';
 
+const title={
+  fontSize: "30px",
+  textAlign: "center"
+}
+
 class App extends Component {
+
+  constructor () {
+    super() 
+    this.state = {
+      name: "Neigbors", 
+      authenticated: false
+    }
+  }
+
+login(){
+  this.setState({
+    authenticated: true
+  })
+}
+
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-        <h1 className="App-title">Neighbors</h1>
-          <img src={background} className="App-background" alt="neigborhood" style={{height: 700, opacity: 0.5}}/>
+        <h1 style={title}>{this.state.name}</h1>
+
+        {this.state.authenticated&&
+        <img src={background} className="App-background" alt="neigborhood" style={{height: 700, opacity: 0.5}}/>
+      }
+
+        {!this.state.authenticated&&
+          <div>
+              <h2> Username: </h2>
+              <input /> 
+              <h2> Password: </h2>
+              <input /> 
+              <br />
+              <br />
+
+              <button onClick={()=>this.login()}> 
+                Login
+              </button>
+              <button> 
+                Sign Up
+              </button>
+          </div>
+        }
+
+        
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+    
       </div>
+
+
     );
   }
 }
