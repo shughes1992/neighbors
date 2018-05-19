52c11570f32f508570c5386c2ef8939781ed9490
Samantha Hughes <hughes2@law.upenn.edu>
2018-05-19 11:40:16 -0400

    sam commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import '../public/stylesheets/style.css'
 import background from './components/assets/images/neighbors-2.jpg'; 
-import './App.css';
+import classes from './App.css';
 
 const title={
   fontSize: "30px",
@@ -26,13 +26,14 @@ login(){
 
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
 
         <header className="App-header">
         <h1 style={title}>{this.state.name}</h1>
 
         {this.state.authenticated&&
         <img src={background} className="App-background" alt="neigborhood" style={{height: 700, opacity: 0.5}}/>
+
       }
 
         {!this.state.authenticated&&
@@ -40,6 +41,7 @@ login(){
               <h2> Username: </h2>
               <input /> 
               <h2> Password: </h2>
+
               <input /> 
               <br />
               <br />
