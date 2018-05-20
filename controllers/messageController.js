e12e7a9d463722945798dfb63ac6cf90608761fb
okputadora <okputadora@gmail.com>
2018-05-20 14:49:58 -0400

    user and message controllers working

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,8 +1,13 @@
+const db = require('../models')
+
 module.exports = {
   get: () => {
     return new Promise((resolve, reject) => {
+      console.log("in message controller")
       db.Message.findAll({})
       .then((messages, err) => {
+        console.log("IN here")
+        console.log(messages)
         if (messages){
           return resolve(messages);
         }
--- models/User.js
+++ models/User.js
@@ -1,9 +1,17 @@
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // Giving the User model a name of type STRING
-    name: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING
+    }
   });
-  
+
   User.associate = function(models) {
     // Associating User with message
     // When an User is deleted, also delete any associated Posts
